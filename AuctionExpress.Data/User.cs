d8274074475b1909aa54390776e415cc550c2a1a
Jeremy Hansen <jhansen1344@gmail.com>
2020-03-05 15:37:55 -0500

    MessyUserClass

--- AuctionExpress.Data/User.cs
+++ AuctionExpress.Data/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,21 @@ namespace AuctionExpress.Data
     public class User
     {
         [Key]
+        
         public int UserId { get; set; }
+        
+        [ForeignKey(nameof(ApplicationUser))]
+        public Guid ForeignUserID { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
         public Guid UserGuid { get; set; }
         [Required]
         public string UserName { get; set; }
         [Required]
         public string Email { get; set; }
+        public bool IsActive { get; set; }
         public string BusinessName { get; set; }
-        [Required]
-        public string Password { get; set; }
 
-        //test
+
+
     }
 }
