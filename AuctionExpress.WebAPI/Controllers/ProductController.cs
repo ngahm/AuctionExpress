3e92ff4d1f9422f4e590bc2bdfbdd7b18a5bfc64
Aaron Barchet <aaron.barchet@gmail.com>
2020-03-05 15:38:19 -0500

    Created Product Service and Controller

--- AuctionExpress.WebAPI/Controllers/ProductController.cs
+++ AuctionExpress.WebAPI/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AuctionExpress.Models;
+using AuctionExpress.Service;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +12,55 @@ namespace AuctionExpress.WebAPI.Controllers
 {
     public class ProductController : ApiController
     {
+        private ProductService CreateProductService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var productService = new ProductService(userId);
+            return productService;
+        }
+
+        //CREATE POST
+        public IHttpActionResult Post(ProductCreate product)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateProductService();
+
+            if (!service.CreateProduct(product))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        //GET POSTS
+        public IHttpActionResult Get()
+        {
+            ProductService productService = CreateProductService();
+            var products = productService.GetProducts();
+            return Ok(products);
+        }
+
+        //GET POSTS BY IT
+        public IHttpActionResult Get(int id)
+        {
+            ProductService productService = CreateProductService();
+            var product = productService.GetProductById(id);
+            return Ok(product);
+        }
+
+        //EDIT POST
+        public IHttpActionResult Put(ProductEdit product)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateProductService();
+
+            if (!service.UpdateProduct(product))
+                return InternalServerError();
+
+            return Ok();
+        }
     }
 }
