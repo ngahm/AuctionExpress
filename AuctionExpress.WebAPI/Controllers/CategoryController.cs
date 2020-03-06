a8f4dc08c49277d009aad861cb39bea4ee61313e
ngahm <nicklaus.gahm@gmail.com>
2020-03-06 16:32:59 -0500

    category

--- AuctionExpress.WebAPI/Controllers/CategoryController.cs
+++ AuctionExpress.WebAPI/Controllers/CategoryController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AuctionExpress.Models;
+using AuctionExpress.Service;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +12,60 @@ namespace AuctionExpress.WebAPI.Controllers
 {
     public class CategoryController : ApiController
     {
+        private CategoryService CreateCategoryService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var categoryService = new CategoryService(userId);
+            return categoryService;
+        }
+
+
+
+
+
+        //GET ALL
+        public IHttpActionResult Get()
+        {
+            CategoryService categoryService = CreateCategoryService();
+            var category = categoryService.GetCategory();
+            return Ok(category);
+        }
+
+        //POST
+        public IHttpActionResult Post(CategoryCreate category)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateCategoryService();
+
+            if (!service.CreateCategory(category))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        //GET BY ID
+        public IHttpActionResult Get(int id)
+        {
+            CategoryService categoryService = CreateCategoryService();
+            var category = categoryService.GetCategoryById(id);
+            return Ok(category);
+        }
+
+
+        //PUT
+        public IHttpActionResult Put(CategoryEdit category)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateCategoryService();
+
+            if (!service.UpdateCategory(category))
+                return InternalServerError();
+
+            return Ok("The inputed strings have successfully updated your ID selection");
+        }
     }
 }
