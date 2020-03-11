d66883a8cacd7ff9d9e4910f9d70ffe9e933819b
ngahm <nicklaus.gahm@gmail.com>
2020-03-11 10:05:02 -0400

    MVC Controller

---
+++ AuctionExpress.WebAPI/Controllers/MVCController.cs
@@ -0,0 +1,45 @@
+﻿using AuctionExpress.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuctionExpress.WebAPI.Controllers
+{
+    public class MVCController : Controller
+    {
+        // GET: MVC
+        public ActionResult Index()
+        {
+            IEnumerable<ProductListItem> productViewer = null; 
+
+            using (var client=new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44320/api/");
+
+                var responseTask = client.GetAsync("product");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<ProductListItem>>();
+                    readTask.Wait();
+
+                    productViewer = readTask.Result;
+                }
+                else      //web api sent error response
+                {         //log response status here.
+                    productViewer = Enumerable.Empty<ProductListItem>();
+
+                    ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+
+                }
+                
+            }
+            return View(productViewer);
+        }
+    }
+}
