67b331fb192ebfda4f84c430193dccfb629f26b1
Aaron Barchet <aaron.barchet@gmail.com>
2020-03-12 16:18:54 -0400

    Updated Display Names

--- AuctionExpress.Models/Product/ProductDetail.cs
+++ AuctionExpress.Models/Product/ProductDetail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,34 @@ namespace AuctionExpress.Models
 {
     public class ProductDetail
     {
+        [Display(Name = "Product Id")]
         public int ProductId { get; set; }
+
+        [Display(Name = "Product Name")]
         public string ProductName { get; set; }
+
+        [Display(Name = "Product Category Id")]
         public int? ProductCategoryId { get; set; }
+
+        [Display(Name = "Product Description")]
         public string ProductDescription { get; set; }
+
+        [Display(Name = "Product Quantity")]
         public int ProductQuantity { get; set; }
+
+        [Display(Name = "Product Is Active")]
         public bool ProductIsActive { get; set; }
+
+        [Display(Name = "Product Start")]
         public DateTimeOffset ProductStartTime { get; set; }
+
+        [Display(Name = "Product Close")]
         public DateTimeOffset ProductCloseTime { get; set; }
+
+        [Display(Name = "Product Seller")]
         public string ProductSeller { get; set; }
+
+        [Display(Name = "Highest Bid")]
         public double HighestBid { get; set; }
     }
 }
--- AuctionExpress.Models/Product/ProductEdit.cs
+++ AuctionExpress.Models/Product/ProductEdit.cs
@@ -9,17 +9,28 @@ namespace AuctionExpress.Models
 {
     public class ProductEdit
     {
+        [Display(Name = "Product Id")]
         public int ProductId { get; set; }
+
+        [Display(Name = "Product Name")]
         [MaxLength(20, ErrorMessage = "Product Name must be less than 20 characters.")]
         [MinLength(2, ErrorMessage = "Product Name must be more than 2 characters.")]
         public string ProductName { get; set; }
+
+        [Display(Name = "Product Category Id")]
         public int? ProductCategoryId { get; set; }
         // public bool ProductIsActive { get; set; }
+
+        [Display(Name = "Product Description")]
         [MaxLength(100, ErrorMessage = "Product Description must be less than 100 characters.")]
         [MinLength(2, ErrorMessage = "Product Description must be more than 2 characters.")]
         public string ProductDescription { get; set; }
+
+        [Display(Name = "Product Quantity")]
         [Range(0, int.MaxValue, ErrorMessage = "Quantity must be a number greater than zero.")]
         public int ProductQuantity { get; set; }
+
+        [Display(Name = "Transaction Id")]
         [DataType(DataType.Date)]
         public DateTimeOffset ProductCloseTime { get; set; }
     }
