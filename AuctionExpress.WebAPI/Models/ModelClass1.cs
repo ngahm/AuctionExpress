bdddd29491dbe8abff583c54031536e2eebc37a1
ngahm <nicklaus.gahm@gmail.com>
2020-03-13 15:28:06 -0400

    deletethis

---
+++ AuctionExpress.WebAPI/Models/ModelClass1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuctionExpress.WebAPI.Models
+{
+    public class ModelClass1
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty2 { get; set; }
+        public int MyProperty3 { get; set; }
+        public int MyProperty4 { get; set; }
+    }
+}
